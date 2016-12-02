6c713a4911d8ddc04e4b88a2c6cd88d663546ce3
Day <dayannefernandesc@gmail.com>
2016-12-02 10:06:28 -0200

    stub bus

---
+++ include/BusStub.hpp
@@ -0,0 +1,64 @@
+/** @file StubBus.hpp
+ *  @brief Stub da camada de negócio.
+ *
+ *  @author Dayanne Fernandes
+ *  @bug Sem bug conhecido.
+ */
+
+#ifndef STUB_BUS_HPP_INCLUDED
+#define STUB_BUS_HPP_INCLUDED
+
+#include "IBus.inl"
+
+class BusStubLogin : public IBusLogin
+{
+public:
+    void autenticar();
+};
+
+class BusStubShared : public IBusShared
+{
+public:
+    string editPersonalInfo(Registration *reg, Name *name);
+    string editPersonalInfo(Registration *reg, Password *pass);
+    string editPersonalInfo(Registration *reg, Phone *phone);
+    string editPersonalInfo(Registration *reg, Email *email);
+    string editPersonalInfo(Registration *reg, Role *role);
+
+    map<string, string> showPersonalInfo(Registration *reqReg, Registration *viewReg);
+};
+
+class BusStubManageManager : public IBusManageManager
+{
+public:
+    string regPM(Registration *reg, ProjectManager *projManager);
+    string delPM(Registration *reg, ProjectManager *projManager);
+};
+
+class BusStubManageDev : public IBusManageDev
+{
+public:
+    string regD(Registration *reg, Developer *dev);
+    string delD(Registration *reg, Developer *dev);
+
+    string addDevToProject(Registration *reg, ProjectCode *projCode, Developer *dev);
+    string delDevFromProject(Registration *reg, ProjectCode *projCode, Developer *dev);
+};
+
+class BusStubManageProject : public IBusManageProject
+{
+public:
+    string regP(Registration *reg, Project *proj);
+    string delP(Registration *reg, Project *proj);
+
+    string editProjectInfo(Registration *reg, ProjectCode *projCode, Name *name);
+    string editProjectInfo(Registration *reg, ProjectCode *projCode, Date *date, string init_end_date);
+    string editProjectInfo(Registration *reg, ProjectCode *projCode, Cost *projCost, string curr_estimate_cost);
+    string editProjectInfo(Registration *reg, ProjectCode *projCode, Developer *dev, Registration *oldDev);
+    string editProjectInfo(Registration *reg, ProjectCode *projCode, ProjectManager *projManager, Registration *oldProjManager);
+    string editProjectInfo(Registration *reg, ProjectCode *projCode,  ProjectState *state);
+
+    map<string, string> showProjectInfo(ProjectCode *projCode);
+};
+
+#endif // STUB_BUS_HPP_INCLUDED
---
+++ src/BusStub.cpp
@@ -0,0 +1,100 @@
+#include "BusStub.hpp"
+
+string BusStubManageManager::regPM(Registration *reg, ProjectManager *projManager){
+    return "OK";
+}
+
+string BusStubManageManager::delPM(Registration *reg, ProjectManager *projManager){
+    return "OK";
+}
+
+string BusStubManageProject::regP(Registration *reg, Project *proj){
+    return "OK";
+}
+
+string BusStubManageProject::delP(Registration *reg, Project *proj){
+    return "OK";
+}
+
+string BusStubManageDev::regD(Registration *reg, Developer *dev){
+    return "OK";
+}
+
+string BusStubManageDev::delD(Registration *reg, Developer *dev){
+    return "OK";
+}
+
+string BusStubShared::editPersonalInfo(Registration *reg, Name *name){
+    return "OK";
+}
+
+string BusStubShared::editPersonalInfo(Registration *reg, Password *pass){
+    return "OK";
+}
+
+string BusStubShared::editPersonalInfo(Registration *reg, Phone *phone){
+    return "OK";
+}
+
+string BusStubShared::editPersonalInfo(Registration *reg, Email *email){
+    return "OK";
+}
+
+string BusStubShared::editPersonalInfo(Registration *reg, Role *role){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode, Name *name){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode, Date *date, string init_end_date){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode, Cost *projCost, string curr_estimate_cost){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode, Developer *dev, Registration *oldDevReg){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode, ProjectManager *projManager,  Registration *oldProjManagerReg){
+    return "OK";
+}
+
+string BusStubManageProject::editProjectInfo(Registration *reg, ProjectCode *projCode,  ProjectState *state){
+    return "OK";
+}
+
+map<string, string> BusStubShared::showPersonalInfo(Registration *reqReg, Registration *viewReg){
+    map<string, string> info;
+
+    info["name"]="Dayanne"
+    info["reg"]="00002"
+    info["pass"]="12345"
+
+    return info;
+}
+
+map<string, string> BusManageProject::showProjectInfo(ProjectCode *projCode){
+    map<string, string> info;
+
+    info["name"]= "Projeto Sem Sentido"
+    info["projcode"]="AAAAA"
+    info["initdate"]="01/12/2020"
+    info["enddate"]="01/12/2026"
+    info["currentcost"]="2000.00"
+    info["estimatecost"]="5000.00"
+
+    return info;
+}
+
+string BusStubManageDev::addDevToProject(Registration *reg, ProjectCode *projCode, Developer *dev){
+    return "OK";
+}
+
+string BusStubManageDev::delDevFromProject(Registration *reg, ProjectCode *projCode, Developer *dev){
+    return "OK";
+}
